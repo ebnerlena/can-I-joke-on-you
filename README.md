b7ee9ebbdd9400374f1fef7e96b3ae5df9a551d8
Lena Ebner <leeena.ebner@gmail.com>
2023-10-31 13:44:46 +0100

    feat: add robot from three.js example

--- README.md
+++ README.md
@@ -16,9 +16,12 @@ A robot that tells jokes to the user. Able to detect the user’s facial express
 - [Presentations](./presentations)
 
 ## Links
-- [General DOcument](https://docs.google.com/document/d/1xoBDYfB_tQNx1Hu9t8IMaJoRUrBUHCxZczYuDr3DBCg/edit#heading=h.rekzzaq41cmi)
+- [General Document](https://docs.google.com/document/d/1xoBDYfB_tQNx1Hu9t8IMaJoRUrBUHCxZczYuDr3DBCg/edit#heading=h.rekzzaq41cmi)
 - [Presentation 01](https://docs.google.com/presentation/d/1JB8wn9jGe2sOnM-HTnyxz9yOSQSLPO_NgbTbA2yuGwo/edit#slide=id.g288f80c3119_0_0)
 
+
+## Commands
+- `npx gltfjsx RobotExpressive.glb --types ` -> create tsx file from .glb model
 
 
 ## Resources
@@ -28,4 +31,7 @@ A robot that tells jokes to the user. Able to detect the user’s facial express
 - Smile Dataset: 
   - https://www.kaggle.com/datasets/ananthu017/emotion-detection-fer
   - https://www.kaggle.com/datasets/chazzer/smiling-or-not-face-data
-
+- Robot Model: https://github.com/mrdoob/three.js/tree/master/examples/models/gltf/RobotExpressive
+- Three.js https://threejs.org/examples/#webgl_animation_skinning_morph
+- Gltfjsx: https://github.com/pmndrs/gltfjsx
+- React Three Fiber: https://www.npmjs.com/package/@react-three/fiber
--- robot_ui/src/App.tsx
+++ robot_ui/src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber'
-import Box from './components/Box';
+import Model from './components/Model';
+import { Plane } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="header">
         <h1>Can I joke on you?</h1>
       </header>
-      <main>
-        <Canvas>
-          <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+      <main className='scene'>
+        <Canvas color="#a0a0a0">
+          <hemisphereLight intensity={3} color="#ffffff" groundColor="#8d8d8d" position={[0, 20, 0]} />
+          <directionalLight intensity={3} color="#ffffff" position={[3, 10, 10]} castShadow shadow-camera-top={2} shadow-camera-bottom={-2} shadow-camera-left={-2} shadow-camera-right={2} shadow-camera-near={0.1} shadow-camera-far={40} />
+
+          <Suspense fallback={<div>Loading model...</div>}>
+            <Model position={[0,-2, 0]} />
+          </Suspense>
+
+          <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -2.1, 0]} receiveShadow />
+
         </Canvas>
       </main>
     </div>
