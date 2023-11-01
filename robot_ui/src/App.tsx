fcf387a4ce389c215dbd4c62aa5c576746f28c19
Lena Ebner <leeena.ebner@gmail.com>
2023-11-01 10:56:25 +0100

    feat: add controls and joke text

--- robot_ui/src/App.tsx
+++ robot_ui/src/App.tsx
@@ -2,7 +2,8 @@ import React, { Suspense } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber'
 import Model from './components/Model';
-import { PerspectiveCamera, Plane } from '@react-three/drei';
+import { PerspectiveCamera, Text } from '@react-three/drei';
+import Controls from './components/Controls';
 
 function App() {
   return (
@@ -11,15 +12,24 @@ function App() {
         <h1>Can I joke on you?</h1>
       </header>
       <main className='scene'>
-        <Canvas color="#a0a0a0">
-        <PerspectiveCamera makeDefault position={[0, 5,15]} fov={75} aspect={window.innerWidth / window.innerHeight} near={0.1} far={1000} rotation={[0, 0, 0]} />
-          <hemisphereLight intensity={3} color="#ffffff" groundColor="#8d8d8d" position={[0, 20, 0]} />
-          <directionalLight intensity={3} color="#ffffff" position={[3, 10, 10]} castShadow shadow-camera-top={2} shadow-camera-bottom={-2} shadow-camera-left={-2} shadow-camera-right={2} shadow-camera-near={0.1} shadow-camera-far={40} />
-          <Suspense fallback={<div>Loading model...</div>}>
-            <Model position={[0, -2, 0]} scale={[3,3, 3]} rotation={[0, Math.PI / 1, 0]}  /> {/* Math.PI / 2 */}
+        <Controls />
+        <Canvas  shadows>
+        <Text
+          position={[0, 24, 0]}
+          fontSize={1.2}
+          color="black"
+          maxWidth={40}
+        >
+          "What do you call a factory that makes okay products?" "A satisfactory."
+     </Text>
+        <PerspectiveCamera makeDefault position={[0, 20, 18]} fov={75} aspect={window.innerWidth / window.innerHeight} near={0.01} far={1000} rotation={[-15 * Math.PI/180, 0, 0]} />
+          <hemisphereLight intensity={2} color="#ffffff" groundColor="#8d8d8d" position={[0, 50, 0]} />
+          <directionalLight intensity={3} color="#ffffff" position={[3, 20, 10]} castShadow shadow-camera-top={2} shadow-camera-bottom={-2} shadow-camera-left={-2} shadow-camera-right={2} shadow-camera-near={0.1} shadow-camera-far={40} />
+          <Suspense>
+            <Model position={[0, 5, 0]} scale={[5,5, 5]} rotation={[0, 178 * Math.PI/180, 0]}  />
           </Suspense>
 
-          <Plane args={[100, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -10, 0]} receiveShadow />
+          {/* <Plane args={[200, 100]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -10, 0]} receiveShadow /> */}
 
         </Canvas>
       </main>
---
+++ robot_ui/src/components/Controls.tsx
@@ -0,0 +1,11 @@
+const Controls = () => {
+    return (
+        <div className="controls">
+            <button className="btn">Play/Pause</button>
+            <button className="btn">Next</button>
+            <button className="btn">Random</button>
+        </div>
+    )
+}
+
+export default Controls;
