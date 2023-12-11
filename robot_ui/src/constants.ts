edae0b6dd01f7f574e9e2a129d25e868275eae82
Lena Ebner <leeena.ebner@gmail.com>
2023-12-11 20:57:54 +0100

    feat: introduce constants

--- robot_ui/src/constants.ts
+++ robot_ui/src/constants.ts
@@ -1,1 +1,11 @@
 export const BACKEND_URL = `http://localhost:${process.env.BACKEND_PORT ?? 5000}`;
+
+export const ROUTES = {
+	HOME: '/',
+	START_NEUTRAL_CALIBRATION: '/start',
+	START_SMILE_CALIBRATION: '/calibration/smile',
+	CALIBRATION: '/calibration',
+	SMILE_CALIBRATION: '/smile-calibration',
+	MAIN: '/main',
+	END: '/end',
+};
