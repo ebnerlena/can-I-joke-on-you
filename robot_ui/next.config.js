81eb97476e8d12f4f835684d8d1d9f83a7a21d26
Lena Ebner <leeena.ebner@gmail.com>
2023-12-13 22:56:54 +0100

    fix: remove standalone server

--- robot_ui/next.config.js
+++ robot_ui/next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: false,
-	output: 'standalone',
-	// async rewrites() {
-	// 	return [
-	// 		{
-	// 			source: '/api/:path*',
-	// 			destination: `http://${process.env.NEXT_PUBLIC_BACKEND_HOST}:${process.env.NEXT_PUBLIC_BACKEND_PORT}/:path*`,
-	// 		},
-	// 	];
-	// },
 };
 
 module.exports = nextConfig;
